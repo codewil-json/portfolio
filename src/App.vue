7ac27e672b98c6a06e821fdbc3543490f4d1bd58
Wil <codewil.json@gmail.com>
2026-07-07 16:13:48 -0300

    Add MyLinks route

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,6 @@
 <template>
-  <NavBar />
-  <HeroSection />
-  <AboutSection />
-  <SkillsSection />
-  <ServicesSection />
-  <ProcessTimeLine />
-  <FeaturedProjects />
-  <ContactSection />
-  <FooterSection />
+  <router-view />
 </template>
-
-<script>
-import NavBar from './components/NavBar.vue'
-import HeroSection from './components/HeroSection.vue'
-import AboutSection from './components/AboutSection.vue'
-import SkillsSection from './components/SkillsSection.vue'
-import ServicesSection from './components/ServicesSection.vue'
-import ProcessTimeLine from './components/ProcessTimeLine.vue'
-import FeaturedProjects from './components/FeaturedProjects.vue'
-import ContactSection from './components/ContactSection.vue'
-import FooterSection from './components/FooterSection.vue'
-
-export default {
-  name: 'App',
-  components: {
-    NavBar,
-    HeroSection,
-    AboutSection,
-    SkillsSection,
-    ServicesSection,
-    ProcessTimeLine,
-    FeaturedProjects,
-    ContactSection,
-    FooterSection
-  }
-}
-</script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Geist:ital,wght@0,100..900;1,100..900&display=swap');
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
+  <NavBar />
+  <HeroSection />
+  <AboutSection />
+  <SkillsSection />
+  <ServicesSection />
+  <ProcessTimeLine />
+  <FeaturedProjects />
+  <ContactSection />
+  <FooterSection />
 </template>
+
+<script>
+import NavBar from '../components/NavBar.vue'
+import HeroSection from '../components/HeroSection.vue'
+import AboutSection from '../components/AboutSection.vue'
+import SkillsSection from '../components/SkillsSection.vue'
+import ServicesSection from '../components/ServicesSection.vue'
+import ProcessTimeLine from '../components/ProcessTimeLine.vue'
+import FeaturedProjects from '../components/FeaturedProjects.vue'
+import ContactSection from '../components/ContactSection.vue'
+import FooterSection from '../components/FooterSection.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+    HeroSection,
+    AboutSection,
+    SkillsSection,
+    ServicesSection,
+    ProcessTimeLine,
+    FeaturedProjects,
+    ContactSection,
+    FooterSection
+  }
+}
+</script>
