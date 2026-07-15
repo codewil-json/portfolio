ecae6007eb000243bf262ca2c245bee4db984bba
Wil | Software Engineer • AI Systems <codewil.json@gmail.com>
2026-07-15 16:12:11 -0300

    Update redirect URL in abrirSite method

--- src/views/MyLinks.vue
+++ src/views/MyLinks.vue
@@ -6,7 +6,7 @@
 export default {
   methods: {
     abrirSite () {
-      window.location.href = 'https://l.instagram.com/?u=https%3A%2F%2Fcodewil-json.github.io%2Flinks%2F%3Futm_source%3Dig%26utm_medium%3Dsocial%26utm_content%3Dlink_in_bio%26fbclid%3DPAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQPOTM2NjE5NzQzMzkyNDU5AAGnLDrIQ42qnaKuT_7ploFPOfmQyJ0e9i0Wggom-_TmyondB9pmcWfUtSvxFhI_aem_o3FGRafp7uVyKiZoGQ5KsQ&e=AUA9alLP97xBzqWfu0Eq50sMr2tks_5RN3mw_inkKjoy1hE5MPSe9AfGC-WBaCHNP_27e2Jx25vSW6b1l29SlXtQxlofftnrn-00t5KBgTIkA3EDBHbxVXA9lXhHrbsUbivtuH1aG8YzGnI'
+      window.location.href = 'https://codewil-json.github.io/links'
     }
   },
   mounted () {
