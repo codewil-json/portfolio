4a484778d550f1972a1223189753d2c198ff25e5
Wil <codewil.json@gmail.com>
2026-07-16 13:41:18 -0300

    gtag

--- src/router/index.js
+++ src/router/index.js
@@ -19,4 +19,12 @@ const router = createRouter({
   routes
 })
 
+router.afterEach((to) => {
+  if (window.gtag) {
+    window.gtag('config', 'G-PE6XKDX06W', {
+      page_path: to.fullPath
+    })
+  }
+})
+
 export default router
